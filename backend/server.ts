8365949396ead242a0ec7076ec79439ecaaa02e7
joeygaitan <joeygaitan707@gmail.com>
2021-02-25 18:35:57 -0800

    setup basic backend. Added in typescript, knex, express, and postgress

---
+++ backend/server.ts
@@ -0,0 +1,35 @@
+import * as express from 'express';
+import * as http from 'http';
+import * as WebSocket from 'ws';
+
+
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+// const {verifyToken} = require('./utils/verify_token')
+
+
+// const signUpRoute = require('./MVC/routes/signup')
+// const loginRoute =  require('./MVC/routes/login')
+// const logOutRoute = require('./MVC/routes/logout')
+
+const router = express.Router();
+const app = express();
+
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+  }
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+const port = process.env.PORT || 5000
+
+//start our server
+app.listen(port, () => {
+    console.log(`Server started on port ${port} :)`);
+});
+
+module.exports = app
